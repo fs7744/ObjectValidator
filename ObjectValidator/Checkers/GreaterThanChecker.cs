ce01c0b310c614aad6e09f961e058824a1491432
Victor.X.Qu <fs7744@hotmail.com>
2016-02-04 10:36:08 +0800

    Create GreaterThanChecker.cs

---
+++ ObjectValidator/Checkers/GreaterThanChecker.cs
@@ -0,0 +1,25 @@
+using ObjectValidator.Interfaces;
+
+namespace ObjectValidator.Checkers
+{
+    public class GreaterThanChecker<T> : BaseChecker<T, int>
+    {
+        private int m_Value;
+
+        public GreaterThanChecker(int value)
+        {
+            m_Value = value;
+        }
+
+        public override IValidateResult Validate(IValidateResult result, int value, string name, string error)
+        {
+            if (value <= m_Value)
+            {
+                AddFailure(result, name, value,
+                    error ?? string.Format("The value must greater than {0}", m_Value));
+            }
+
+            return result;
+        }
+    }
+}
